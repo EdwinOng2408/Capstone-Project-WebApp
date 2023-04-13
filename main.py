83a480b3924d45ff32ba50ffc50384487feb76a7
Yi Rui YI <82104627+yirui23@users.noreply.github.com>
2023-04-13 04:59:23 +0000

    added index.html and styles.css files

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+app.run("0.0.0.0")
