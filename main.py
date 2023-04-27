d4f409509abe90c1b5180e5b2e8b0c551edc49ab
yirui23 <82104627+yirui23@users.noreply.github.com>
2023-04-27 16:03:28 +0000

    added view_students.html and search_record.html with test data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # test comment
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
@@ -24,6 +25,7 @@ def add_cca():
 @app.route("/confirm_cca", methods=["POST", "GET"])
 def confirm_cca():
     if "confirm" in request.args:
+        
         return render_template("add_cca.html",
                                form_meta = {
                                   "action": "/register_cca?registered",
@@ -56,38 +58,41 @@ def add_activity():
                            
                           form_meta = {
                               "action": "/confirm_activity?confirm",
-                              "methods": "POST"},
+                              "method": "post"},
                            
                           form_data = {
-                              "decription": "",
+                              "description": "",
                               "start_date": "",
                               "end_date": ""
                           },
                            
-                          type = "add"
+                          type = "new"
                           )
 
 @app.route("/confirm_activity", methods = ["POST", "GET"])
 def confirm_activity():
-    if "confirm" in request.args():
+    if "confirm" in request.args:
         return render_template("add_activity.html",
                               
-                              form_meta = {
-                                  "action": "/register_cca?registered",
-                                  "method": "POST"},
-
-                              form_data = {
+                               form_meta = {
+                                  "action": "/register_activity?registered",
+                                  "method": "post"},
+                               
+                               form_data = {
                                    "start_date": request.form["start_date"],
                                    "end_date": request.form["end_date"],
-                                    "description": request.form["description"]
-                              }
+                                   "description": request.form["description"]
+                              },
                                
                               type = "confirm"
                               )
 
 @app.route("/register_activity", methods=["POST", "GET"])
 def register_activity():
-    if "registered" in request.args():
+
+    # jun xiang my brother
+    
+    if "registered" in request.args:
         return render_template("add_activity.html",
 
                                form_data = {
@@ -97,6 +102,52 @@ def register_activity():
                                },
 
                                type="registered"
+                              )
+
+
+@app.route("/view_students")
+def view_students():
+
+    #jun xiang my brother
+    #------- test data start
+    data = []
+    with open("test_data.csv", "r") as f:
+        reader = csv.DictReader(f)
+        for record in reader:
+            record["View more/Edit"] = record["name"]
+            data.append(record)
+    #-------- test data end
+    
+    return render_template("view_students.html",
+                           record_data = data
+                          )
+
+
+@app.route("/search_record")
+def search_record():
+
+    if "student_name" in request.args:
+        return render_template("search_record.html",
+                               form_meta = {
+                                   "action": "dmadmasdad", #banana
+                                   "method": "dakadadad"},
+                               
+                               form_data = {
+                                   "student_name": request.args["student_name"]},
+                               
+                               type = "search"
+                              )
+
+    else:
+        return render_template("search_record.html",
+                               form_meta = {
+                                   "action": "/search_record",
+                                   "method": "get"},
+                               
+                               form_data = {
+                                   "student_name": ""},
+    
+                               type = "new"
                               )
                                
 app.run("0.0.0.0")
